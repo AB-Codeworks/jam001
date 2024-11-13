277bec5878eab45d90a57e6eaa65c31cedeab9c2
AB-Codeworks <96074172+AB-Codeworks@users.noreply.github.com>
2024-11-12 19:26:11 -0500

    + I should note that the theme of the game is not fully decided, this art may change + Adjusted game position in browser + Added images for hold progress bar + Added Gaposis Solid BRK font files, a public domain font + Code rough-in for side passages + End of path exceptions caught + UI areas now on side + UI text that responds to controls + Primary and secondary controls tap and hold input works, only primary tap actually has working code responding to it as yet though. + Moved some common wall drawing code to a function, reducing redundant code.

--- public/main.js
+++ public/main.js
@@ -4,6 +4,8 @@ let colCyan = 0x00ffff;
 let colMagenta = 0xff00ff;
 let colYellow = 0xffff00;
 let curPosition = 0;
+let primaryHoldTimer = null;
+let secondaryHoldTimer = null;
 
 let mainSequence = 
 [
@@ -44,8 +46,24 @@ let mainSequence =
     [true, true, true],    
 ];
 
+let sidePassages = 
+[
+    [
+        [true, false, true],
+        [true, false, true],
+        [true, true, true]
+    ],
+    [
+        [true, false, true],
+        [true, true, true]
+    ]
+];
+
 gameScene.preload = function()
 {
+    this.downKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+    this.upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    this.load.bitmapFont('gaposis', 'GaposisSolidBRK.png', 'GaposisSolidBRK.xml');
     this.load.image('ceiling', 'ceil_a.jpg');
     this.load.image('ceiling_layer', 'ceil_b.png');
     this.load.image('floor', 'flr_a.jpg');
@@ -53,18 +71,89 @@ gameScene.preload = function()
     this.load.image('wall_front', 'wall_a_front.png');
     this.load.image('wall_left', 'wall_a_left.png');
     this.load.image('wall_right', 'wall_a_right.png');
-
+    this.load.image('bar', 'foo bar.png');
+    this.load.image('fill', '1x1.png');
+    
     this.input.keyboard.on
-    ('keydown', function (event) {
-        if (event.key === 'ArrowUp')
+    ('keyup', function(event) {
+        if (event.key === 'ArrowUp' && primaryHoldTimer != null) 
         {
             curPosition++;
+            this.scene.flashText(this.scene.primaryText);
             this.scene.drawDungeon();
         }
     });
+    
+    this.input.keyboard.on
+    ('keyup', function(event) {
+        if (event.key === 'ArrowDown' && secondaryHoldTimer != null) 
+        {
+            this.scene.resetSecondary();            
+            this.scene.flashText(this.scene.secondaryText);
+            // TODO attack or w/e the action is
+        }
+    });
+    
+    // TODO touch solution
 
     this.allWalls = this.add.group();
+};
+
+gameScene.flashText = function(text)
+{
+    text.setTint(colYellow);
+    setTimeout(function(text) { text.setTint(colCyan); }, 125, text);
 }
+
+gameScene.resetPrimary = function()
+{
+    if (primaryHoldTimer) 
+    {
+        this.primaryHoldTween.stop();
+        this.primaryHoldTween.remove();
+        this.primaryFill.setScale(0, 28);
+        clearTimeout(primaryHoldTimer);
+        primaryHoldTimer = null;
+    }
+}
+
+gameScene.resetSecondary = function()
+{
+    if (secondaryHoldTimer) 
+    {
+        this.secondaryHoldTween.stop();
+        this.secondaryHoldTween.remove();
+        this.secondaryFill.setScale(0, 28);
+        clearTimeout(secondaryHoldTimer);
+        secondaryHoldTimer = null;
+    }
+}
+
+gameScene.update = function() 
+{
+    if (Phaser.Input.Keyboard.JustDown(this.upKey)) 
+    {
+        primaryHoldTimer = setTimeout(function(context) {
+            context.resetPrimary();
+            // TODO Execute Special Command
+        }, 1000, this);
+
+        this.primaryHoldTween = this.tweens.add({ targets: this.primaryFill, scaleX: 148, duration: 1000 });
+    }
+    else if (Phaser.Input.Keyboard.JustUp(this.upKey)) 
+        this.resetPrimary();
+    else if (Phaser.Input.Keyboard.JustDown(this.downKey)) 
+    {
+        secondaryHoldTimer = setTimeout(function(context) { 
+            context.resetSecondary();
+            // TODO Summon Action Menu
+        }, 1000, this);
+
+        this.secondaryHoldTween = this.tweens.add({ targets: this.secondaryFill, scaleX: 148, duration: 1000 });
+    }
+    else if (Phaser.Input.Keyboard.JustUp(this.downKey)) 
+        this.resetSecondary();
+};
 
 gameScene.create = function()
 {
@@ -85,285 +174,195 @@ gameScene.create = function()
     flr_lyr.setPosition(200, 540);
     flr_lyr.setTint(colMagenta);
     this.drawDungeon();
-}
+    
+    let t2 = this.add.bitmapText(100, 20, 'gaposis', 'Primary:\nUp\nArrow\n\n\nTap:', 24, 1);
+    t2.setOrigin(0.5, 0);
+    t2.setTint(colCyan);
 
-//gameScene.update = function()
-//{
-    //if (this.input.keyboard.checkDown(this.input.keyboard.addKey('LEFT'), 250))
-//}
+    let t = this.add.bitmapText(100, 20 + t2.height, 'gaposis', 'Advance', 24, 1);
+    t.setOrigin(0.5, 0);
+    t.setTint(colCyan);
+    this.primaryText = t;
+
+    let s = this.add.sprite(100, 120 + t2.height, 'bar');
+    s.setOrigin(0.5, 0);
+    s.setTint(colCyan);
+
+    s = this.add.sprite(26, 102 + t.height + t2.height, 'fill');
+    s.setOrigin(0, 0);
+    s.setScale(0, 28); // 148 = full width
+    s.setTint(colCyan);
+    this.primaryFill = s;
+
+    t = this.add.bitmapText(100, 150 + t.height + t2.height, 'gaposis', 'Hold:\nView Map', 24, 1);
+    t.setOrigin(0.5, 0);
+    t.setTint(colCyan);
+    this.primaryHoldText = t;
+
+    t2 = this.add.bitmapText(1820, 20, 'gaposis', 'Secondary:\nDown\nArrow\n\n\nTap:', 24, 1);
+    t2.setOrigin(0.5, 0);
+    t2.setTint(colCyan);
+
+    t = this.add.bitmapText(1820, 20 + t2.height, 'gaposis', 'Attack', 24, 1);
+    t.setOrigin(0.5, 0);
+    t.setTint(colCyan);
+    this.secondaryText = t;
+
+    s = this.add.sprite(1820, 120 + t2.height, 'bar');
+    s.setOrigin(0.5, 0);
+    s.setTint(colCyan);
+
+    s = this.add.sprite(1746, 102 + t.height + t2.height, 'fill');
+    s.setOrigin(0, 0);
+    s.setScale(0, 28); // 148 = full width
+    s.setTint(colCyan);
+    this.secondaryFill = s;
+    
+    t = this.add.bitmapText(1820, 150 + t.height + t2.height, 'gaposis', 'Hold:\nAction\nMenu', 24, 1);
+    t.setOrigin(0.5, 0);
+    t.setTint(colCyan);
+};
+
+gameScene.addWall = function(xPos, yPos, type, tint, xScale, yScale)
+{
+    let wall = this.add.sprite(0, 0, type);
+    this.allWalls.add(wall);
+    wall.setOrigin(0, 0);
+    wall.setScale(xScale, yScale);
+    wall.setPosition(xPos, yPos);
+    wall.setTint(tint);
+};
 
 gameScene.drawDungeon = function()
 {
     this.allWalls.children.each(function(wall) { wall.destroy(); });
-    let wall, wallScale;
 
-    // TODO don't draw them if they are obscured
-    // TODO method to reduce redundancy
+    // TODO optimization (maybe) don't draw them if they are obscured - need to work out those conditions
+    // TODO update text if there's a side passage
 
-    // 4 forward in mainSequence
-    if (mainSequence[curPosition + 4][0])
+    try
     {
-        wall = this.add.sprite(0, 0, 'wall_left');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.08);
-        wall.setPosition(924, 508);
-        wall.setTint(colMagenta);
-
-        if (!mainSequence[curPosition + 3][0])
+        // 4 forward in mainSequence
+        if (mainSequence[curPosition + 4][0])
         {
-            wall = this.add.sprite(0, 0, 'wall_front');
-            this.allWalls.add(wall);
-            wall.setOrigin(0, 0);
-            wall.setScale(0.12);
-            wall.setPosition(863, 508);
-            wall.setTint(colMagenta);
-        }
-    } 
-    else        
-    {
-        wall = this.add.sprite(0, 0, 'wall_front');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.05);
-        wall.setPosition(919, 532);
-        wall.setTint(colMagenta);
-    }
-
-    if (mainSequence[curPosition + 4][1])
-    {
-        wall = this.add.sprite(0, 0, 'wall_front');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.06, 0.05);
-        wall.setPosition(944, 532);
-        wall.setTint(colMagenta);
-    }
-    else
-    {
-        wall = this.add.sprite(0, 0, 'wall_front');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.06, 0.05);
-        wall.setPosition(944, 532);
-        wall.setTint(colBlack);
-    }
+            this.addWall(924, 508, 'wall_left', colMagenta, 0.08, 0.08);
     
-    if (mainSequence[curPosition + 4][2])
-    {
-        wall = this.add.sprite(0, 0, 'wall_right');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.08);
-        wall.setPosition(974, 508);
-        wall.setTint(colMagenta);
-
-        if (!mainSequence[curPosition + 3][2])
-        {
-            wall = this.add.sprite(0, 0, 'wall_front');
-            this.allWalls.add(wall);
-            wall.setOrigin(0, 0);
-            wall.setScale(0.12);
-            wall.setPosition(998, 508);
-            wall.setTint(colMagenta);
-        }
-    }    
-    else        
-    {
-        wall = this.add.sprite(0, 0, 'wall_front');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.05);
-        wall.setPosition(972, 532);
-        wall.setTint(colMagenta);
-    }    
-
-    // 3 forward in mainSequence
-    if (mainSequence[curPosition + 3][0])
-    {
-        wall = this.add.sprite(0, 0, 'wall_left');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.16);
-        wall.setPosition(878, 476);
-        wall.setTint(colMagenta);
-
-        if (!mainSequence[curPosition + 2][0])
-        {
-            wall = this.add.sprite(0, 0, 'wall_front');
-            this.allWalls.add(wall);
-            wall.setOrigin(0, 0);
-            wall.setScale(0.25);
-            wall.setPosition(750, 476);
-            wall.setTint(colMagenta);
-        }
-    }
-
-    if (mainSequence[curPosition + 3][1])
-    {
-        wall = this.add.sprite(0, 0, 'wall_front');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.14, 0.11);
-        wall.setPosition(924, 512);
-        wall.setTint(colMagenta);
-    }
+            if (!mainSequence[curPosition + 3][0])
+                this.addWall(863, 508, 'wall_front', colMagenta, 0.12, 0.12);
+        } 
+        else        
+            this.addWall(919, 532, 'wall_front', colMagenta, 0.05, 0.05);
     
-    if (mainSequence[curPosition + 3][2])
-    {
-        wall = this.add.sprite(0, 0, 'wall_right');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.16);
-        wall.setPosition(994, 476);
-        wall.setTint(colMagenta);
-
-        if (!mainSequence[curPosition + 2][2])
-        {
-            wall = this.add.sprite(0, 0, 'wall_front');
-            this.allWalls.add(wall);
-            wall.setOrigin(0, 0);
-            wall.setScale(0.25);
-            wall.setPosition(1040, 476);
-            wall.setTint(colMagenta);
-        }
-    }    
-    
-    // 2 forward in mainSequence
-    if (mainSequence[curPosition + 2][0])
-    {
-        wall = this.add.sprite(0, 0, 'wall_left');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.32);
-        wall.setPosition(784, 410);
-        wall.setTint(colMagenta);
-
-        if (!mainSequence[curPosition + 1][0])
-        {
-            wall = this.add.sprite(0, 0, 'wall_front');
-            this.allWalls.add(wall);
-            wall.setOrigin(0, 0);
-            wall.setScale(0.48);
-            wall.setPosition(538, 410);
-            wall.setTint(colMagenta);
-        }
-    }
-
-    if (mainSequence[curPosition + 2][1])
-    {
-        wall = this.add.sprite(0, 0, 'wall_front');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.5, 0.24);
-        wall.setPosition(878, 476);
-        wall.setTint(colMagenta);
-    }
-    
-    if (mainSequence[curPosition + 2][2])
-    {
-        wall = this.add.sprite(0, 0, 'wall_right');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.32);
-        wall.setPosition(1040, 410);
-        wall.setTint(colMagenta);
-
-        if (!mainSequence[curPosition + 1][2])
-        {
-            wall = this.add.sprite(0, 0, 'wall_front');
-            this.allWalls.add(wall);
-            wall.setOrigin(0, 0);
-            wall.setScale(0.48);
-            wall.setPosition(1134, 410);
-            wall.setTint(colMagenta);
-        }
-    }    
+        if (mainSequence[curPosition + 4][1])
+            this.addWall(944, 532, 'wall_front', colMagenta, 0.06, 0.05);
+        else
+            this.addWall(944, 532, 'wall_front', colBlack, 0.06, 0.05);
         
-    // 1 forward in mainSequence
-    if (mainSequence[curPosition + 1][0])
-    {
-        wall = this.add.sprite(0, 0, 'wall_left');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.64);
-        wall.setPosition(596, 280);
-        wall.setTint(colMagenta);
-
-        if (!mainSequence[curPosition][0])
+        if (mainSequence[curPosition + 4][2])
         {
-            wall = this.add.sprite(0, 0, 'wall_front');
-            this.allWalls.add(wall);
-            wall.setOrigin(0, 0);
-            wall.setScale(0.78, 1);
-            wall.setPosition(200, 280);
-            wall.setTint(colMagenta);
+            this.addWall(974, 508, 'wall_right', colMagenta, 0.08, 0.08);
+    
+            if (!mainSequence[curPosition + 3][2])
+                this.addWall(998, 508, 'wall_front', colMagenta, 0.12, 0.12);
+        }    
+        else        
+            this.addWall(972, 532, 'wall_front', colMagenta, 0.05, 0.05);
+    }
+    catch (e) { console.log(e); }
+
+    try
+    {        
+        // 3 forward in mainSequence
+        if (mainSequence[curPosition + 3][0])
+        {
+            this.addWall(878, 476, 'wall_left', colMagenta, 0.16, 0.16);
+    
+            if (!mainSequence[curPosition + 2][0])
+                this.addWall(750, 476, 'wall_front', colMagenta, 0.25, 0.25);
+        }
+    
+        if (mainSequence[curPosition + 3][1])
+            this.addWall(924, 512, 'wall_front', colMagenta, 0.14, 0.11);
+        
+        if (mainSequence[curPosition + 3][2])
+        {
+            this.addWall(994, 476, 'wall_right', colMagenta, 0.16, 0.16);
+    
+            if (!mainSequence[curPosition + 2][2])
+                this.addWall(1040, 476, 'wall_front', colMagenta, 0.25, 0.25);
+        }    
+    }
+    catch (e) { console.log(e); }
+
+    try
+    {        
+        // 2 forward in mainSequence
+        if (mainSequence[curPosition + 2][0])
+        {
+            this.addWall(784, 410, 'wall_left', colMagenta, 0.32, 0.32);
+    
+            if (!mainSequence[curPosition + 1][0])
+                this.addWall(538, 410, 'wall_front', colMagenta, 0.48, 0.48);
+        }
+    
+        if (mainSequence[curPosition + 2][1])
+            this.addWall(878, 476, 'wall_front', colMagenta, 0.5, 0.24);
+        
+        if (mainSequence[curPosition + 2][2])
+        {
+            this.addWall(1040, 410, 'wall_right', colMagenta, 0.32, 0.32);
+    
+            if (!mainSequence[curPosition + 1][2])
+                this.addWall(1134, 410, 'wall_front', colMagenta, 0.48, 0.48);
+        }    
+    }
+    catch (e) { console.log(e); }
+
+    try
+    {        
+        // 1 forward in mainSequence
+        if (mainSequence[curPosition + 1][0])
+        {
+            this.addWall(596, 280, 'wall_left', colMagenta, 0.64, 0.64);
+    
+            if (!mainSequence[curPosition][0])
+                this.addWall(200, 280, 'wall_front', colMagenta, 0.78, 1);
+        }
+    
+        if (mainSequence[curPosition + 1][1])
+            this.addWall(784, 410, 'wall_front', colMagenta, 0.69, 0.48);
+    
+        if (mainSequence[curPosition + 1][2])
+        {
+            this.addWall(1134, 280, 'wall_right', colMagenta, 0.64, 0.64);
+    
+            if (!mainSequence[curPosition][2])
+                this.addWall(1322, 280, 'wall_front', colMagenta, 0.78, 1);
         }
     }
+    catch (e) { console.log(e); }
 
-    if (mainSequence[curPosition + 1][1])
-    {
-        wall = this.add.sprite(0, 0, 'wall_front');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.69, 0.48);
-        wall.setPosition(784, 410);
-        wall.setTint(colMagenta);
+    try
+    {        
+        // Foremost
+        if (mainSequence[curPosition][0])
+            this.addWall(200, 0, 'wall_left', colMagenta, 1.35, 1.35);
+    
+        if (mainSequence[curPosition][1])
+            this.addWall(596, 280, 'wall_front', colMagenta, 1.42, 1);
+    
+        if (mainSequence[curPosition][2])
+            this.addWall(1322, 0, 'wall_right', colMagenta, 1.35, 1.35);    
     }
-
-    if (mainSequence[curPosition + 1][2])
-    {
-        wall = this.add.sprite(0, 0, 'wall_right');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(0.64);
-        wall.setPosition(1134, 280);
-        wall.setTint(colMagenta);
-
-        if (!mainSequence[curPosition][2])
-        {
-            wall = this.add.sprite(0, 0, 'wall_front');
-            this.allWalls.add(wall);
-            wall.setOrigin(0, 0);
-            wall.setScale(0.78, 1);
-            wall.setPosition(1322, 280);
-            wall.setTint(colMagenta);
-        }        
+    catch (e) 
+    { 
+        console.log(e); 
+        curPosition--;
+        this.drawDungeon();
+        this.resetPrimary();
+        alert('End of dungeon!');
     }
-
-    // Foremost
-    if (mainSequence[curPosition][0])
-    {
-        wall = this.add.sprite(0, 0, 'wall_left');
-        this.allWalls.add(wall);
-        wallScale = 1080 / wall.height;
-        wall.setOrigin(0, 0);
-        wall.setPosition(200, 0);
-        wall.setScale(wallScale);
-        wall.setTint(colMagenta);
-    }        
-
-    if (mainSequence[curPosition][1])
-    {
-        wall = this.add.sprite(0, 0, 'wall_front');
-        this.allWalls.add(wall);
-        wall.setOrigin(0, 0);
-        wall.setScale(1.42, 1);
-        wall.setPosition(596, 280);
-        wall.setTint(colMagenta);
-    }
-
-    if (mainSequence[curPosition][2])
-    {
-        wall = this.add.sprite(0, 0, 'wall_right');
-        this.allWalls.add(wall);
-        wallScale = 1080 / wall.height;
-        wall.setOrigin(0, 0);
-        wall.setScale(wallScale);
-        wall.setPosition(1720 - (wall.width * wallScale), 0);
-        wall.setTint(colMagenta);
-    }    
-}
+};
 
 let config = 
 {
